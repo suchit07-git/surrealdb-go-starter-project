e2dab110e97f0e066c286dc1724e79bc52805ac6
suchit07-git <suchitkarunakaran@gmail.com>
2023-11-05 15:08:20 +0530

    Added return statement

--- pkg/database/delete.go
+++ pkg/database/delete.go
@@ -3,10 +3,11 @@ package database
 import "log/slog"
 
 func (d Database) Delete(what string) {
-	slog.Debug("ID passed to select", what)
+	slog.Debug("ID passed to delete", what)
 	data, err := d.db.Delete(what)
 	if err != nil {
 		slog.Error("Unable to delete data in table: %v", err)
 	}
 	slog.Info("Successfully deleted data in table", data)
+	return
 }
